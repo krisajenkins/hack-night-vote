dc1365c65e9eb7ebd2ff6c6f41309369b5d53c67
Kris Jenkins <krisajenkins@gmail.com>
2016-05-23 20:04:34 +0100

    Tweaks.

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -10,8 +10,10 @@ module Lib
 
 import           Control.Lens        hiding (from, (^.))
 import           Data.Aeson
-import           Data.ByteString
+import           Data.ByteString     (ByteString)
+import           Data.Set            as Set
 import           Data.Text
+import           Data.UUID
 import           GHC.Generics
 import           Snap
 import           Snap.CORS
@@ -19,31 +21,49 @@ import           Snap.Util.FileServe
 import           Snap.Util.GZip
 import           Utils
 
-
 -- App
-data App =
-  App {}
-
+data App = App
 makeLenses ''App
 
-data Hack =
-  Hack {id   :: Int
-       ,name :: Text}
+data Hacker =
+  Hacker {hackerId   :: UUID
+         ,hackerName :: Text}
   deriving (Show,Eq,Generic,ToJSON,FromJSON)
 
-hackList :: [Hack]
-hackList =
-  [Hack {id = 1
-        ,name = "ISS SDR"}
-  ,Hack {id = 2
-        ,name = "Hack Night Voting System"}
-  ,Hack {id = 3
-        ,name = "A*"}]
+data Hack =
+  Hack {hackId   :: Int
+       ,hackName :: Text
+       ,votes    :: Set UUID}
+  deriving (Show,Eq,Generic,ToJSON,FromJSON)
+
+instance FromJSON UUID where
+  parseJSON =
+    withText "UUID"
+             (\t ->
+                case fromText t of
+                  Nothing -> fail (unpack t)
+                  Just uuid -> return uuid)
+
+
+instance ToJSON UUID where
+  toJSON = String . toText
+
+initialHackList :: [Hack]
+initialHackList =
+  [Hack {hackId = 1
+        ,hackName = "ISS SDR"
+        ,votes = Set.empty}
+  ,Hack {hackId = 2
+        ,hackName = "Hack Night Voting System"
+        ,votes = Set.empty}
+  ,Hack {hackId = 3
+        ,hackName = "A*"
+        ,votes = Set.empty}]
 
 hackListHandler :: Handler App App [Hack]
 hackListHandler =
   method GET $
-     return hackList
+     return initialHackList
 
 routes :: [(ByteString, Handler App App ())]
 routes = [("",serveDirectory "static"),("/api",apiRoutes)]
